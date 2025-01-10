61d41e5b7d881d431178b1f2586105c7751e434e
Reuben Strobel <fts192pb@students.codeinstitute.net>
2025-01-10 18:18:16 +0000

    remove game grid divs as it will be more efficient to create  with JS

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,3 +2,21 @@ animalEmoji = ['🐯','🐳','🐻','🦙','🦭','🦘','🦑','🐼','🐙','�
 foodEmoji = ['','','','','','','','','','','','','','','','','','','','','','','','']
 faceEmoji = ['','','','','','','','','','','','','','','','','','','','','','','','']
 
+// console.log($("#emoji-buttons").children());
+
+rowCounter = 1;
+colCounter = 1;
+emojiIndex = 0
+
+function assignEmojiButtons() {
+    for (let row = 1; row > 3;  row++) {
+        for (let col = 1; col === $(".emoji .flex-container").children().length;  col++) {
+            console.log(`#emoji-row-${rowCounter} > .box-${colCounter}`)
+            $(`#emoji-row-${rowCounter} > .box-${colCounter}`).innerText = animalEmoji[emojiIndex];
+            emojiIndex++;
+            colCounter++;
+        }
+        rowCounter++;
+        colCounter = 1;
+    }
+}
