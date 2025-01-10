c3d9f07c0214f827bb726aa598a056133c1b25f1
Reuben Strobel <fts192pb@students.codeinstitute.net>
2025-01-10 19:00:32 +0000

    Add DOMContentLoaded event listener to document

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,24 @@
-// Emojis taken from emojipedia
-const animalEmoji = ['🐯','🐳','🐻','🦙','🦭','🦘','🦑','🐼','🐙','🐨','🦊','🦝','🐓','🦚','🦩','🐬','🐝','🦈','🪼','🐧','🦆','🐡','🦉','🦔']
-const foodEmoji = ['🥑','🍫','🍒','🍉','🥦','🍆','🍊','🍬','🫐','🥕','🍋','🫑','🍌','🍍','🥬','🍎','🍇','🍑','🥪','🌶️','🍕','🧀','🍔','🍩']
-const faceEmoji = ['😀','🤩','😜','😁','🤑','😂','🥰','🤔','🤤','🥳','😶‍🌫️','😴','🥶','😰','🥹','😡','😑','👿','🤗','😚','😎','🙃','🤒','😫']
+document.addEventListener('DOMContentLoaded', function() {
+    // Emojis taken from emojipedia
+    const animalEmoji = ['🐯','🐳','🐻','🦙','🦭','🦘','🦑','🐼','🐙','🐨','🦊','🦝','🐓','🦚','🦩','🐬','🐝','🦈','🪼','🐧','🦆','🐡','🦉','🦔']
+    const foodEmoji = ['🥑','🍫','🍒','🍉','🥦','🍆','🍊','🍬','🫐','🥕','🍋','🫑','🍌','🍍','🥬','🍎','🍇','🍑','🥪','🌶️','🍕','🧀','🍔','🍩']
+    const faceEmoji = ['😀','🤩','😜','😁','🤑','😂','🥰','🤔','🤤','🥳','😶‍🌫️','😴','🥶','😰','🥹','😡','😑','👿','🤗','😚','😎','🙃','🤒','😫']
+    let chosenEmoji
 
-// console.log($("#emoji-buttons").children());
 
-emojiIndex = 0
-function assignEmojiButtons() {
-    for (let row = 1; row < 4;  row++) {
-        for (let col = 1; col !== $(`#emoji-row-${row}`).children().length; col++) {
-            console.log($(`#emoji-row-${row} > .box-${col}`));
-            $(`#emoji-row-${row} > .box-${col}`).text(animalEmoji[emojiIndex]);
-            emojiIndex++;
+
+    chosenEmoji = animalEmoji // temporary for testing, will come from modal selection in future
+    emojiIndex = 0
+    function assignEmojiButtons() { // Assigns the emojis to their respective buttons based on which category was selected
+        for (let row = 1; row < 4;  row++) {
+            for (let col = 1; col !== $(`#emoji-row-${row}`).children().length; col++) {
+                console.log($(`#emoji-row-${row} > .box-${col}`));
+                $(`#emoji-row-${row} > .box-${col}`).text(chosenEmoji[emojiIndex]);
+                emojiIndex++;
+            }
         }
     }
-}
 
-assignEmojiButtons()
+    assignEmojiButtons()
+
+}
