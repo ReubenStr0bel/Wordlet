f09e6f3cafa84aa18c3f557b87aa07ec2a54693e
Reuben Strobel <fts192pb@students.codeinstitute.net>
2025-01-10 21:08:17 +0000

    Add event listeners on backspace and submit buttons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -30,7 +30,6 @@
                 box.text(chosenEmoji[emojiIndex]);
                 emojiIndex++;
                 box.on("click", function () {
-                    // console.log("Button clicked");
                     $(`#${gameRow}-${gameCol}`).text($(this).text());
                     if (gameCol <= 5) { // Incrementing gameCol to 6 will mean that button presses
                         gameCol++       // won't change the final box once the row is full
@@ -55,3 +54,10 @@
     }
 
     // Assign event listeners to backspace and submit buttons
+    $("#backspace").on("click", function () {
+        
+    });
+    $("#submit").on("click", function () {
+
+    });
+    
