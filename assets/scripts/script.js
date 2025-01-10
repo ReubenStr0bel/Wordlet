072c87f0a11fae758ce593719344327dd50e1602
Reuben Strobel <fts192pb@students.codeinstitute.net>
2025-01-10 19:13:02 +0000

    Add for loop to create the game grid boxes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,9 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
     chosenEmoji = animalEmoji // temporary for testing, will come from modal selection in future
     emojiIndex = 0
     function assignEmojiButtons() { // Assigns the emojis to their respective buttons based on which category was selected
-        for (let row = 1; row < 4;  row++) {
+        for (let row = 1; row <= $("#emoji-buttons").children().length;  row++) {
             for (let col = 1; col !== $(`#emoji-row-${row}`).children().length; col++) {
-                console.log($(`#emoji-row-${row} > .box-${col}`));
                 $(`#emoji-row-${row} > .box-${col}`).text(chosenEmoji[emojiIndex]);
                 emojiIndex++;
             }
@@ -20,5 +19,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     assignEmojiButtons()
+
+    // Create the game grid
+    for (let row = 1; row <= 6; row++) {
+        for (let col = 1; col <= 5; col++) {
+            let box = document.createElement("span");
+            box.id = `${row}-${col}`;
+            box.classList.add("box");
+            $("#game-grid").append(box);
+        }
+    }
 
 })
