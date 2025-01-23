d389e382ab931ef365a41cf14928cba4413a4110
Reuben Strobel <fts192pb@students.codeinstitute.net>
2025-01-23 18:32:32 +0000

    Add message in tutorial modal about win counter reset

--- assets/tests/script.test.js
+++ assets/tests/script.test.js
@@ -4,7 +4,7 @@
 const { test, expect } = require("@jest/globals");
 const { updateKeyboard } = require("../scripts/script.js");
 const { describe } = require("yargs");
-const $ = require('https://code.jquery.com/jquery-3.7.1.js');
+// const $ = require('https://code.jquery.com/jquery-3.7.1.js');
 
 beforeAll(() => {
     let fs = require("fs");
